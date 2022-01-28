5cf5cbad72cad0a64b7595d3c02407e6e8eb1486
Grant12345z <62903954+Grant12345z@users.noreply.github.com>
2022-01-28 17:46:08 -0500

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,29 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    Key = 2
+    tiles.setTileAt(location, assets.tile`myTile7`)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile6`, function (sprite, location) {
     if (Key == 1) {
-        game.over(true)
+        tiles.setTilemap(tilemap`level5`)
+        tiles.placeOnRandomTile(mySprite, assets.tile`myTile2`)
+    } else if (false) {
+        if (Key == 2) {
+            tiles.setTilemap(tilemap`level1`)
+            tiles.placeOnRandomTile(mySprite, assets.tile`myTile2`)
+        }
+    } else if (false) {
+        if (Key == 3) {
+            game.over(true)
+        }
     } else {
-        mySprite.sayText("I need a key")
+        mySprite.sayText("I need a key", 3000, false)
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`key`, function (sprite, location) {
+    Key = 3
+    tiles.setTileAt(location, assets.tile`myTile7`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, location) {
     Key = 1
     tiles.setTileAt(location, assets.tile`myTile7`)
 })
@@ -33,5 +51,5 @@ mySprite = sprites.create(img`
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
-tiles.setTilemap(tilemap`level1`)
+tiles.setTilemap(tilemap`level4`)
 tiles.placeOnRandomTile(mySprite, assets.tile`myTile2`)
