f5414b1ea0a5adfac9558f6621b6092bcf838648
Grant12345z <62903954+Grant12345z@users.noreply.github.com>
2022-01-31 18:38:33 -0500

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,45 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile6`, function (sprite, l
         tiles.setTilemap(tilemap`level5`)
         effects.confetti.startScreenEffect(1500)
         tiles.placeOnRandomTile(mySprite, assets.tile`myTile2`)
+        list = [sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 8 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy), sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 8 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)]
+        Spots1 = [[2, 16], [31, 12]]
+        for (let index = 0; index <= list.length - 1; index++) {
+            tiles.placeOnTile(list[index], tiles.getTileLocation(Spots1[index][0], Spots1[index][1]))
+        }
     } else {
         mySprite.sayText("I need a key", 3000, false)
     }
@@ -16,6 +55,62 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile9`, function (sprite, l
         tiles.setTilemap(tilemap`level1`)
         effects.confetti.startScreenEffect(1500)
         tiles.placeOnRandomTile(mySprite, assets.tile`myTile2`)
+        list = [sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 8 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy), sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 8 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy), sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 8 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)]
+        Spots1 = [[23, 25], [2, 15], [41, 33]]
+        for (let index = 0; index <= list.length - 1; index++) {
+            tiles.placeOnTile(list[index], tiles.getTileLocation(Spots1[index][0], Spots1[index][1]))
+        }
     } else {
         mySprite.sayText("I need a key", 3000, false)
     }
@@ -33,6 +128,89 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile10`, function (sprite, 
         tiles.setTilemap(tilemap`level13`)
         effects.confetti.startScreenEffect(1500)
         tiles.placeOnRandomTile(mySprite, assets.tile`myTile2`)
+        list = [
+        sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 8 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy),
+        sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 8 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy),
+        sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 8 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy),
+        sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 8 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . 8 8 1 1 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+        ]
+        Spots1 = [
+        [10, 8],
+        [29, 6],
+        [52, 30],
+        [39, 41]
+        ]
+        for (let index = 0; index <= list.length - 1; index++) {
+            tiles.placeOnTile(list[index], tiles.getTileLocation(Spots1[index][0], Spots1[index][1]))
+        }
     } else {
         mySprite.sayText("I need a key", 3000, false)
     }
@@ -48,8 +226,10 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     }
 })
 let Key = 0
-let SixerVar = 0
+let Spots1: number[][] = []
+let list: Sprite[] = []
 let mySprite: Sprite = null
+let SixerVar = 0
 scene.setBackgroundColor(6)
 game.splash("press the spacebar or the ", "A button to continue")
 game.splash("Ready player One", "Independent Reading Project Two")
@@ -59,6 +239,8 @@ game.splash("There are four levels")
 game.splash("In the fourth level ", "you only need to find the egg")
 game.splash("Use the arrow keys or ", "W,A,S, and D to move")
 game.splash("Game by ", "Grant Snow")
+tiles.setTilemap(tilemap`level4`)
+SixerVar = 1
 mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -79,26 +261,8 @@ mySprite = sprites.create(img`
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
-tiles.setTilemap(tilemap`level4`)
-SixerVar = 1
-let list = [sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . 8 8 8 8 . . . . . . 
-    . . . . . 8 8 1 1 8 8 . . . . . 
-    . . . . . 8 8 1 8 8 8 . . . . . 
-    . . . . . 8 8 1 1 8 8 . . . . . 
-    . . . . . 8 8 1 1 8 8 . . . . . 
-    . . . . . . 8 8 8 8 . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Enemy), sprites.create(img`
+tiles.placeOnRandomTile(mySprite, assets.tile`myTile2`)
+list = [sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -116,16 +280,20 @@ let list = [sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Enemy)]
-for (let value of list) {
-	
+Spots1 = [[1, 8]]
+for (let index = 0; index <= list.length - 1; index++) {
+    tiles.placeOnTile(list[index], tiles.getTileLocation(Spots1[index][0], Spots1[index][1]))
 }
 game.onUpdate(function () {
-    let Sixer: Sprite = null
     if (SixerVar == 1) {
-        Sixer.y += 1
+        for (let value2 of list) {
+            value2.y += 1
+        }
     } else {
         if (SixerVar == 3) {
-            Sixer.y += -1
+            for (let value3 of list) {
+                value3.y += -1
+            }
         }
     }
 })
